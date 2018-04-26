c928f6d0f6da3ea80177b61ff588a49c8b6bb026
Andrej Mancevski <andrej@edusoft2.com>
2018-04-26 16:52:14 +0200

    Room pages modified to contain room collapseble tabs

---
+++ src/pages.js
@@ -0,0 +1,9 @@
+﻿var modalDis = function (imgClicked, imgModal, modal) {
+    var img = imgClicked;
+
+    modal.style.display = 'block';
+    imgModal.src = img.getAttribute('src');
+}
+var closeModal = function (modalToBeClosed) {
+    modalToBeClosed.style.display = 'none';
+}
