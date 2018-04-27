90fa5056f86bc2efbd3b2ff9db9a3584c855d2c8
Andrej Mancevski <andrej@edusoft2.com>
2018-04-27 16:58:44 +0200

    Finished the hotel pages and room tabs. Started working on calendar

---
+++ src/DatePicker.js
@@ -0,0 +1,26 @@
+﻿var dateCompare = function (newDate) {
+    var d1 = document.getElementById('dateSelected1').innerHTML.split(" ");
+    var d2 = document.getElementById('dateSelected2').innerHTML.split(" ");
+    var 
+    
+};
+
+var dateSelect = function (dateA) {
+    var date = dateA.innerHTML;
+    var dateDisplay = document.getElementById("dateSelected1");
+    //check if a date has been selected
+    //and if yes is it before the new date
+    if (dateDisplay.innerHTML !== ""){
+        dateDisplay = document.getElementById("dateSelected2");
+    }
+    if (dateDisplay.innerHTML !== "") {
+        //2 dates have already been selected
+        //compare the new date 
+
+    }
+    console.log(date.text);
+    var dateMonth = document.getElementById("month").innerHTML;
+    dateDisplay.innerHTML = date + " " + dateMonth;
+};
+
+var checkDateSelected
--- src/User.js
+++
@@ -1,6 +0,0 @@
-﻿function User(params) {
-    this.firstName = params.firstName;
-    this.lastName = params.lastName;
-    this.email = params.email;
-    this.phoneNo = params.phoneNo;
-}
