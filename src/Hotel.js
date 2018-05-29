94f9a4349837dffc68db13379bea70a9be2be89d
Andrej Mancevski <andrej@edusoft2.com>
2018-05-29 16:48:32 +0200

    Finished Room to string and Date storage

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,4 +1,16 @@
-﻿function Hotel(slots) {
+﻿function Hotel (slots) {
     this.name = slots.name;
-    this.rooms = slots.rooms;
+    this.rooms = [];
+    if (slots.rooms !== undefined) {
+        this.rooms = slots.rooms;
+    }
+}
+Hotel.prototype.toString = function () {
+    var str = "";
+    str += "Hotel " + this.name + " has the following rooms:";
+
+    this.rooms.forEach(function (room) {
+        str += room.toString() + "\n";
+    });
+    return str;
 }
--- src/Room.js
+++ src/Room.js
@@ -1,14 +1,16 @@
 ﻿function Room (slots) {
     this.name = slots.name;
     this.reservedDates = [];
+    this.originalId = ++Room.globalIds;
 }
 Room.prototype.reserveRoom = function (dateStart, dateEnd) {
     if (checkValidDateInterval(dateStart, dateEnd)) {
+        //console.log("if passed");
         y = dateStart.getFullYear();
         m = dateStart.getMonth();
         d = dateStart.getDate();
         var dateTemp = new Date(y, m, d);
-        while (dateTemp.getTime < dateEnd.getTime) {
+        while (dateTemp.getTime() < dateEnd.getTime()) {
             this.reservedDates.push(dateTemp);
             dateTemp.setDate(dateTemp.getDate() + 1);
         }
@@ -19,7 +21,32 @@ Room.prototype.reserveRoom = function (dateStart, dateEnd) {
     }
 };
 var checkValidDateInterval = function (dateStart, dateEnd) {
-    if (dateStart < dateEnd)
+    if (dateStart.getTime() < dateEnd.getTime()) {
         return true;
-    else return false;
+    }
+    else { return false; }
+}
+Room.globalIds = "1000";
+
+Room.prototype.toString = function () {
+    var str = "";
+    str += "Name: " + this.name + "\n";
+    str += "RoomID: " + this.originalId + "\n";
+    if (this.reservedDates.length == 0) {
+        str += "This room has not been reserved";
+    } else {
+        str += "Dates reserved: \n";
+        this.reservedDates.forEach (function (date) {
+            str += date + "\n";
+        });
+    }
+    return str; 
+}
+
+/*Getters and setters*/
+Room.prototype.getId = function () {
+    return this.originalId;
+}
+Room.prototype.getName = function () {
+    return this.name;
 }
